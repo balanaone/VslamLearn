ed2a99c7919d103b08a3fc61c000423a1d930b03
QiangLi <clyz4liqiang@163.com>
2018-12-19 00:26:41 +0800

    Frame completed

--- include/myslam/frame.h
+++ include/myslam/frame.h
@@ -14,8 +14,8 @@ public:
     typedef std::shared_ptr<Frame> Ptr;
     unsigned long           id_;
     double                  time_stamp_;
-    SE3                     T_c_w;
-    Camera::Ptr             camera;
+    SE3                     T_c_w_;
+    Camera::Ptr             camera_;
     Mat                     color_, depth_;
 
 public:
@@ -23,8 +23,18 @@ public:
     Frame(long id, double time_stamp=0, SE3 T_c_w=SE3(), Camera::Ptr camera=nullptr, Mat color=Mat(), Mat depth=Mat());
     ~Frame();
 
+    // factory function
+    static Frame::Ptr createFrame();
 
-}
+    // find the depth in depth map
+    double findDepth(const cv::KeyPoint & kp);
+
+    // Get camera center
+    Vector3d getCamCenter() const;
+
+    // check if a point is in this frame
+    bool isInFrame(const Vector3d & pt_world);
+};
 
 }
 
